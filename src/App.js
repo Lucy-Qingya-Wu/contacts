b12e1775d4a83d2c3a0c5062e7acbbb7d2b72229
Lucy-Qingya-Wu <lucy.wu000@gmail.com>
2017-10-28 17:30:12 -0400

    upload all files in react project called contacts

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class ContactList extends Component{
+
+  render(){
+    const people = this.props.contacts;
+
+    return <ol>
+               {people.map(person=><li key={person.name}>{person.name}</li>)}
+           </ol>
+  }
+
+
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <ContactList contacts = {[
+          {name:"Sam"},
+          {name:"Lucy"},
+          {name:"Tom"}
+        ]}/>
+        <ContactList contacts = {[
+          {name:"Rose"},
+          {name:"Mike"},
+          {name:"James"}
+        ]}/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
